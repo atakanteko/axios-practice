7a07ee777cd993cb767b4979ad9228ad81db26a0
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-02-06 12:13:10 +0300

    change structure of axios service

--- src/service/initializer.js
+++ src/service/initializer.js
@@ -1,9 +1,9 @@
 import service from "./service";
 
-export const createHttpRequest = async ({ method, endpoint, path, config }) => {
+export const createHttpRequest = async ({ endpoint, path, config }) => {
   try {
     const url = path ? `${endpoint}/${path}` : endpoint;
-    const response = await service[method](url, { config });
+    const response = await service["request"]({ url, ...config });
     return response.data;
   } catch (error) {
     return error;
